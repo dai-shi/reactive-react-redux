140ab0278672e2a00d37275bfae91779711365d8
daishi <daishi@axlight.com>
2018-11-17 15:35:49 +0900

    add immer example

---
+++ examples/04_immer/Person.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+
+import { bailOutHack, useReduxDispatch, useReduxState } from '../../src/index';
+
+import { Action, State } from './state';
+
+const TextBox: React.SFC<{ text: string }> = ({ text }) => {
+  // tslint:disable-next-line:no-console
+  console.log('rendering text:', text);
+  return <span>{text}</span>;
+};
+
+const PersonFirstName = bailOutHack(() => {
+  const state = useReduxState<State>([]);
+  const dispatch = useReduxDispatch<Action>();
+  return (
+    <div>
+      First Name:
+      <TextBox text={state.person.firstName} />
+      <input
+        value={state.person.firstName}
+        onChange={(event) => {
+          const firstName = event.target.value;
+          dispatch({ firstName, type: 'setFirstName' });
+        }}
+      />
+    </div>
+  );
+});
+
+const PersonLastName = bailOutHack(() => {
+  const state = useReduxState<State>([]);
+  const dispatch = useReduxDispatch<Action>();
+  return (
+    <div>
+      Last Name:
+      <TextBox text={state.person.lastName} />
+      <input
+        value={state.person.lastName}
+        onChange={(event) => {
+          const lastName = event.target.value;
+          dispatch({ lastName, type: 'setLastName' });
+        }}
+      />
+    </div>
+  );
+});
+
+const PersonAge = bailOutHack(() => {
+  const state = useReduxState<State>([]);
+  const dispatch = useReduxDispatch<Action>();
+  return (
+    <div>
+      Age:
+      <input
+        value={state.person.age}
+        onChange={(event) => {
+          const age = Number(event.target.value) || 0;
+          dispatch({ age, type: 'setAge' });
+        }}
+      />
+    </div>
+  );
+});
+
+const Person = () => (
+  <>
+    <PersonFirstName />
+    <PersonLastName />
+    <PersonAge />
+  </>
+);
+
+export default Person;
---
+++ examples/04_immer/state.ts
@@ -0,0 +1,29 @@
+import { produce } from 'immer';
+
+const initialState = {
+  counter: 0,
+  person: {
+    age: 0,
+    firstName: '',
+    lastName: '',
+  },
+};
+
+export type State = typeof initialState;
+
+export type Action =
+  | { type: 'increment' }
+  | { type: 'decrement' }
+  | { type: 'setFirstName', firstName: string }
+  | { type: 'setLastName', lastName: string }
+  | { type: 'setAge', age: number };
+
+export const reducer = (state = initialState, action: Action) => produce(state, (draft) => {
+  switch (action.type) {
+    case 'increment': draft.counter += 1; break;
+    case 'decrement': draft.counter -= 1; break;
+    case 'setFirstName': draft.person.firstName = action.firstName; break;
+    case 'setLastName': draft.person.lastName = action.lastName; break;
+    case 'setAge': draft.person.age = action.age; break;
+  }
+});
