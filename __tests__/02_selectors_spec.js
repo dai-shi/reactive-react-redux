653f8fc5d6b002953132cbfa736adafb536e197c
daishi <daishi@axlight.com>
2019-04-10 21:37:34 +0900

    another test (which is failing now)

--- __tests__/02_selectors_spec.js
+++ __tests__/02_selectors_spec.js
@@ -74,4 +74,54 @@ describe('selectors spec', () => {
     expect(numOfRenders2).toBe(2);
     expect(container).toMatchSnapshot();
   });
+
+  it('should re-render with no false positives', () => {
+    const initialState = {
+      counter1: 0,
+      counter2: 0,
+    };
+    const reducer = (state = initialState, action) => {
+      if (action.type === 'increment1') {
+        return { ...state, counter1: state.counter1 + 1 };
+      }
+      if (action.type === 'increment2') {
+        return { ...state, counter2: state.counter2 + 1 };
+      }
+      return state;
+    };
+    const store = createStore(reducer);
+    let numOfRenders1 = 0;
+    const Counter1 = () => {
+      numOfRenders1 += 1;
+      const { c1 } = useReduxSelectors({
+        c1: useCallback(state => ({ isBig: state.counter1 > 2 }), []),
+        c2: useCallback(state => ({ isBig: state.counter2 > 2 }), []),
+      });
+      const dispatch = useReduxDispatch();
+      return (
+        <div>
+          <span>{c1.isBig ? 'big' : 'not big'}</span>
+          <button type="button" onClick={() => dispatch({ type: 'increment1' })}>inc1</button>
+        </div>
+      );
+    };
+    const App = () => (
+      <StrictMode>
+        <ReduxProvider store={store}>
+          <Counter1 />
+          <Counter1 />
+        </ReduxProvider>
+      </StrictMode>
+    );
+    const { getByText, container } = render(<App />);
+    expect(numOfRenders1).toBe(4); // doubled because of StrictMode
+    expect(container).toMatchSnapshot();
+    fireEvent.click(getByText('inc1'));
+    expect(numOfRenders1).toBe(4);
+    fireEvent.click(getByText('inc1'));
+    expect(numOfRenders1).toBe(4);
+    fireEvent.click(getByText('inc1'));
+    expect(numOfRenders1).toBe(8);
+    // expect(container).toMatchSnapshot();
+  });
 });
