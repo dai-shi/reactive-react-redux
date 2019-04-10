f0bb2976fbbaab9a274f013bf8d9177a80a10ceb
daishi <daishi@axlight.com>
2019-04-10 21:15:19 +0900

    add test for selectors

---
+++ __tests__/02_selectors_spec.js
@@ -0,0 +1,75 @@
+import React, { useCallback, StrictMode } from 'react';
+import { createStore } from 'redux';
+
+import { render, fireEvent, cleanup } from 'react-testing-library';
+
+import {
+  ReduxProvider,
+  useReduxSelectors,
+  useReduxDispatch,
+} from '../src/index';
+
+describe('selectors spec', () => {
+  afterEach(cleanup);
+
+  it('should re-render on update', () => {
+    const initialState = {
+      counter1: 0,
+      counter2: 0,
+    };
+    const reducer = (state = initialState, action) => {
+      if (action.type === 'increment1') {
+        return { ...state, counter1: state.counter1 + 1 };
+      }
+      if (action.type === 'increment2') {
+        return { ...state, counter2: state.counter2 + 1 };
+      }
+      return state;
+    };
+    const store = createStore(reducer);
+    let numOfRenders1 = 0;
+    const Counter1 = () => {
+      numOfRenders1 += 1;
+      const { c1 } = useReduxSelectors({
+        c1: useCallback(state => ({ value: state.counter1 }), []),
+      });
+      const dispatch = useReduxDispatch();
+      return (
+        <div>
+          <span>{c1.value}</span>
+          <button type="button" onClick={() => dispatch({ type: 'increment1' })}>inc1</button>
+        </div>
+      );
+    };
+    let numOfRenders2 = 0;
+    const Counter2 = () => {
+      numOfRenders2 += 1;
+      const { c2 } = useReduxSelectors({
+        c2: useCallback(state => ({ value: state.counter2 }), []),
+      });
+      const dispatch = useReduxDispatch();
+      return (
+        <div>
+          <span>{c2.value}</span>
+          <button type="button" onClick={() => dispatch({ type: 'increment2' })}>inc2</button>
+        </div>
+      );
+    };
+    const App = () => (
+      <StrictMode>
+        <ReduxProvider store={store}>
+          <Counter1 />
+          <Counter2 />
+        </ReduxProvider>
+      </StrictMode>
+    );
+    const { getByText, container } = render(<App />);
+    expect(numOfRenders1).toBe(2); // doubled because of StrictMode
+    expect(numOfRenders2).toBe(2);
+    expect(container).toMatchSnapshot();
+    fireEvent.click(getByText('inc1'));
+    expect(numOfRenders1).toBe(4);
+    expect(numOfRenders2).toBe(2);
+    expect(container).toMatchSnapshot();
+  });
+});
