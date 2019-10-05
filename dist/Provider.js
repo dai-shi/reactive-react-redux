c3cd7d265f73b64e6de8cd7cb4b26de0cb1da2da
daishi <daishi@axlight.com>
2019-10-05 12:48:17 +0900

    run compile

--- dist/Provider.js
+++ dist/Provider.js
@@ -7,7 +7,13 @@ exports.Provider = exports.defaultContext = exports.createCustomContext = void 0
 
 var _react = require("react");
 
-var _utils = require("./utils");
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 // -------------------------------------------------------
 // context
@@ -49,7 +55,13 @@ var Provider = function Provider(_ref) {
       _ref$customContext = _ref.customContext,
       customContext = _ref$customContext === void 0 ? defaultContext : _ref$customContext,
       children = _ref.children;
-  var forceUpdate = (0, _utils.useForceUpdate)();
+
+  var _useReducer = (0, _react.useReducer)(function (c) {
+    return c + 1;
+  }, 0),
+      _useReducer2 = _slicedToArray(_useReducer, 2),
+      forceUpdate = _useReducer2[1];
+
   var state = store.getState();
   var listeners = (0, _react.useRef)([]); // we call listeners in render intentionally.
   // listeners are not technically pure, but
@@ -76,7 +88,7 @@ var Provider = function Provider(_ref) {
       forceUpdate();
     });
     return unsubscribe;
-  }, [store, forceUpdate]);
+  }, [store]);
   return (0, _react.createElement)(customContext.Provider, {
     value: {
       state: state,
