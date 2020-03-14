f1ad83043d933b0f34d9170572f846580a5f8e8a
daishi <daishi@axlight.com>
2020-03-14 10:00:26 +0900

    export type only

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { PatchedStore, patchStore } from './patchStore';
+export type { PatchedStore } from './patchStore';
+export { patchStore } from './patchStore';
 export { useSelector } from './useSelector';
 export { useTrackedState } from './useTrackedState';
 export { trackMemo, getUntrackedObject } from 'proxy-compare';
