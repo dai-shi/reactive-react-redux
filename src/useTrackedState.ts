c7a52fcbf91de543ee5e9670053c88f1854107f1
daishi <daishi@axlight.com>
2020-03-16 18:23:25 +0900

    improve useTrackedState with scoped variables

--- src/useTrackedState.ts
+++ src/useTrackedState.ts
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/ban-ts-ignore */
 
 import {
-  useCallback,
+  useMemo,
   // @ts-ignore
   useMutableSource,
-  useRef,
 } from 'react';
 import {
   Action as ReduxAction,
@@ -19,7 +18,7 @@ import {
 } from 'proxy-compare';
 
 import { PatchedStore, subscribe } from './patchStore';
-import { useIsomorphicLayoutEffect, useAffectedDebugValue } from './utils';
+import { useAffectedDebugValue } from './utils';
 
 const MODE_ALWAYS_ASSUME_CHANGED_IF_UNAFFECTED = 0;
 const MODE_ALWAYS_ASSUME_UNCHANGED_IF_UNAFFECTED = (
@@ -51,42 +50,38 @@ export const useTrackedState = <State, Action extends ReduxAction<any>>(
 ) => {
   const { mutableSource } = patchedStore;
   const affected = new WeakMap();
-  const lastTracked = useRef<any>();
-  const getSnapshot = useCallback((store: Store<State, Action>) => {
-    const nextState = store.getState();
-    const lastTrackedCurrent = lastTracked.current;
-    if (!lastTrackedCurrent) return nextState;
-    if (lastTrackedCurrent.affected !== affected) return nextState;
-    if (lastTrackedCurrent.state === nextState) return nextState;
-    if (isDeepChanged(
-      lastTrackedCurrent.state,
-      nextState,
-      lastTrackedCurrent.affected,
-      lastTrackedCurrent.cache,
-      lastTrackedCurrent.mode,
-    )) return nextState;
-    // not changed
-    return lastTrackedCurrent.state;
-  }, [affected]);
-  const state: State = useMutableSource(mutableSource, getSnapshot, subscribe);
-  useIsomorphicLayoutEffect(() => {
-    lastTracked.current = {
-      state,
-      affected,
-      cache: new WeakMap(),
-      /* eslint-disable no-nested-ternary, indent */
-      mode:
-      opts.unstable_forceUpdateForStateChange ? MODE_ALWAYS_ASSUME_CHANGED_IF_UNAFFECTED
-      : opts.unstable_ignoreIntermediateObjectUsage ? MODE_ALWAYS_ASSUME_UNCHANGED_IF_UNAFFECTED
-      : opts.unstable_ignoreStateEquality ? MODE_MUTABLE_ROOT_STATE
-      : /* default */ MODE_DEFAULT,
-      /* eslint-enable no-nested-ternary, indent */
+  const deepChangedMode = (
+    /* eslint-disable no-nested-ternary, indent, no-multi-spaces */
+      opts.unstable_forceUpdateForStateChange     ? MODE_ALWAYS_ASSUME_CHANGED_IF_UNAFFECTED
+    : opts.unstable_ignoreIntermediateObjectUsage ? MODE_ALWAYS_ASSUME_UNCHANGED_IF_UNAFFECTED
+    : opts.unstable_ignoreStateEquality           ? MODE_MUTABLE_ROOT_STATE
+    : /* default */                                 MODE_DEFAULT
+    /* eslint-enable no-nested-ternary, indent, no-multi-spaces */
+  );
+  const getSnapshot = useMemo(() => {
+    let prevState: State | null = null;
+    const deepChangedCache = new WeakMap();
+    return (store: Store<State, Action>) => {
+      const nextState = store.getState();
+      if (prevState !== null && prevState !== nextState && !isDeepChanged(
+        prevState,
+        nextState,
+        affected,
+        deepChangedCache,
+        deepChangedMode,
+      )) {
+        // not changed
+        return prevState;
+      }
+      prevState = nextState;
+      return nextState;
     };
-  });
+  }, [affected, deepChangedMode]);
+  const state: State = useMutableSource(mutableSource, getSnapshot, subscribe);
   if (process.env.NODE_ENV !== 'production') {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useAffectedDebugValue(state, affected);
   }
-  const proxyCache = useRef(new WeakMap()); // per-hook proxyCache
-  return createDeepProxy(state, affected, proxyCache.current);
+  const proxyCache = useMemo(() => new WeakMap(), []); // per-hook proxyCache
+  return createDeepProxy(state, affected, proxyCache);
 };
