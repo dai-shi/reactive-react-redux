46edf6afbd3e7a52dceccf9c5a2083ac652f3111
daishi <daishi@axlight.com>
2020-05-10 20:45:37 +0900

    prefer createElement

--- src/memo.js
+++ src/memo.js
@@ -1,11 +1,11 @@
-import { memo as reactMemo } from 'react';
+import { createElement, memo as reactMemo } from 'react';
 
 import { trackMemo } from './deepProxy';
 
 export const memo = (Component, areEqual) => {
   const WrappedComponent = (props) => {
     Object.values(props).forEach(trackMemo);
-    return Component(props);
+    return createElement(Component, props);
   };
   return reactMemo(WrappedComponent, areEqual);
 };
